6bd483aa35cc47bfeea58198316e83110887a69f
VBTX <jv.svedaite@gmail.com>
2020-03-15 15:37:25 -0500

    add post route for the form

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.post("/compose", (req, res) => {
+  console.log(req.body.postTitle);
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
